afd2affadcb3fca3c58f79cb4acb115ed83c20cc
Camilo Barraza <cbarraza11@gmail.com>
2014-09-07 00:18:33 -0500

    agrega actividades, botones e intents de prueba

--- Parcados/src/com/example/parcados/MainActivity.java
+++ Parcados/src/com/example/parcados/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.parcados;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -11,6 +16,16 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		LinearLayout layout = (LinearLayout) findViewById(R.id.layout) ;
+		
+		for ( int i =  0 ; i < 0 ; i ++ ) { 
+			Button button = new Button(this) ; 
+			button.setText("otro s" + i  ) ; 
+			layout.addView(button) ;
+		}
+		Toast msg = Toast.makeText(this	, "hello covas", 3)  ; 
+		msg.show() ; 
 	}
 
 	@Override
@@ -25,10 +40,19 @@ public class MainActivity extends Activity {
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
+		Toast.makeText(this, "escogiste " + item.getTitle(), 3).show() ;
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	public void irATestActivity ( View  v) 	{ 
+		 Intent intent = new Intent(this, TestActivity.class) ;
+		 startActivity(intent) ;
+	}
+	
+	
+	
 }
