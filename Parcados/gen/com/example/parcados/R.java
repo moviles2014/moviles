6d442fa9a2237d96635d75633d52d43de318918d
Fabio Moyano <fabiomoyano22@gmail.com>
2014-09-13 10:15:38 -0500

    Arreglos a elementos de la interfaz, cambio en las activities para que sea más relativo. Bajé precio y cupos al final del detalle_parqueadero porque los consultaremos por SMS

--- Parcados/gen/com/example/parcados/R.java
+++ Parcados/gen/com/example/parcados/R.java
@@ -27,24 +27,24 @@ public final class R {
     }
     public static final class id {
         public static final int action_settings=0x7f080010;
-        public static final int button1=0x7f08000e;
-        public static final int button2=0x7f08000d;
-        public static final int imageView1=0x7f08000c;
-        public static final int layout=0x7f08000b;
+        public static final int button1=0x7f08000b;
+        public static final int button2=0x7f08000e;
+        public static final int imageView1=0x7f08000d;
+        public static final int layout=0x7f08000c;
         public static final int otraActivityMenu=0x7f080011;
         public static final int otraActivityMenu2=0x7f080012;
         public static final int scrollView1=0x7f08000f;
         public static final int textView1=0x7f080000;
-        public static final int textView10=0x7f080007;
-        public static final int textView11=0x7f080009;
-        public static final int textView2=0x7f080002;
-        public static final int textView3=0x7f080004;
-        public static final int textView4=0x7f080006;
-        public static final int textView5=0x7f080008;
-        public static final int textView6=0x7f08000a;
-        public static final int textView7=0x7f080001;
-        public static final int textView8=0x7f080003;
-        public static final int textView9=0x7f080005;
+        public static final int textView10=0x7f080003;
+        public static final int textView11=0x7f080005;
+        public static final int textView2=0x7f08000a;
+        public static final int textView3=0x7f080008;
+        public static final int textView4=0x7f080002;
+        public static final int textView5=0x7f080004;
+        public static final int textView6=0x7f080006;
+        public static final int textView7=0x7f080009;
+        public static final int textView8=0x7f080007;
+        public static final int textView9=0x7f080001;
     }
     public static final class layout {
         public static final int activity_detalle_parqueadero=0x7f030000;
@@ -64,13 +64,20 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int button_name1=0x7f050003;
         public static final int calcularPrecio=0x7f05000c;
+        public static final int consultarCupos=0x7f05000d;
         public static final int hello_world=0x7f050001;
         public static final int otraACt=0x7f05000a;
         public static final int otraAct=0x7f050009;
-        public static final int parqueaderos=0x7f05000d;
+        public static final int parqueadero_caracteristicas=0x7f050013;
+        public static final int parqueadero_cupos=0x7f050010;
+        public static final int parqueadero_direccion=0x7f050014;
+        public static final int parqueadero_horario=0x7f050012;
+        public static final int parqueadero_precio=0x7f050011;
+        public static final int parqueaderos=0x7f05000e;
         public static final int testText=0x7f050008;
         public static final int text=0x7f050004;
         public static final int textview=0x7f050006;
+        public static final int zonas=0x7f05000f;
     }
     public static final class style {
         /** 
