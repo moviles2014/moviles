0c66309b33fadba5b17a1ee4ac4a86dfd8172b79
Fabio Moyano <fabiomoyano22@gmail.com>
2014-10-14 10:32:36 -0500

    no message

--- Proyecto/02 - Iteración 2/Parcados Android/src/parcados/activities/BusquedaParqueaderosActivity.java
+++ Proyecto/02 - Iteración 2/Parcados Android/src/parcados/activities/BusquedaParqueaderosActivity.java
@@ -18,7 +18,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
