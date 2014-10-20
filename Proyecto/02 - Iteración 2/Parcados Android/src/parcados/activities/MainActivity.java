b94d9db1d6a0e6a576afef19a1a78d5f7ed12fef
Camilo Barraza <cbarraza11@gmail.com>
2014-10-20 14:57:56 -0500

    App para los modificar los datos de los parqueaderos (falta implementar el broadcast del cambio)

--- Proyecto/02 - Iteración 2/Parcados Android/src/parcados/activities/MainActivity.java
+++ Proyecto/02 - Iteración 2/Parcados Android/src/parcados/activities/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
 	/**
 	 * Cuando se crea la aplicación
 	 */
-	@Override
+	@Override 
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
