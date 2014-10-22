7d3459231ba935acc2e39cc05a85397bd505d798
Fabio Moyano <fabiomoyano22@gmail.com>
2014-10-21 20:29:06 -0500

    Revert "Proyecto iOS"
    This reverts commit 0475cd6fb698b0bfa7a4a5d32d9b8f88eec5db91.

--- Proyecto/02 - Iteración 2/google_play_services/samples/cast/tictactoe/gen/com/google/android/gms/cast/samples/tictactoe/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.android.gms.cast.samples.tictactoe;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- Proyecto/02 - Iteración 2/google_play_services/samples/drive/playground/gen/com/google/android/gms/drive/sample/realtimeplayground/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.google.android.gms.drive.sample.realtimeplayground;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
